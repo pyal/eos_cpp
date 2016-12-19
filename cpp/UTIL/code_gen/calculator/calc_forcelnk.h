a13e8bb33e369a601dc6412423bcb6e15ab03ed7
Alexey <pyal@mail.ru>
2016-12-18 23:32:10 -0800

    adding forcelnk files

---
+++ cpp/UTIL/code_gen/calculator/calc_forcelnk.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "util/code_gen/calculator/calc_prg_types.h"
+extern ForceLinkVoid<StdClcDouble> Dummy_StdClcDouble;
+extern ForceLinkVoid<StdClcUndefined> Dummy_StdClcUndefined;
+extern ForceLinkVoid<StdClcString> Dummy_StdClcString;
