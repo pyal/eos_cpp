8cd1ba8c43de0bcb7caf7567360ad4eada171101
Alexey <pyal@mail.ru>
2017-03-18 03:12:15 -0700

    Adding logging

--- cpp/URS/urs_curve_main.cpp
+++ cpp/URS/urs_curve_main.cpp
@@ -13,8 +13,6 @@ void DrawCurve(int argc, const char *argv[]) {
     in >> curve;
     if(!curve)
         throw info_except("Could not read URS_Curve class from the <%s> file\n", argv[1]);
-    //fcout.SetRefMask(SavableClass::UnlinkedObjects);
-    //fcout<<curve;
     curve->Do();
 }
 
@@ -23,54 +21,56 @@ void DrawCurve(int argc, const char *argv[]) {
 #include "urs_forcelnk.h"
 
 Stroka GenerateDetailedHelp() {
-    Stroka res = Stroka("Program build time ") + __TIMESTAMP__ + "\n";
-    res += Stroka(" Registered curves:\n\n\n") +
+    Stroka res = Stroka(" Registered curves:\n\n\n") +
            SavableClass::HelpForCategory("URS_Curve_category");
     res += Stroka(" Registered EOS FreeE:\n\n\n") +
            SavableClass::HelpForCategory("MatterFreeE_category");
     res += Stroka(" Registered EOS caloric:\n\n\n") +
            SavableClass::HelpForCategory("Matter_category");
 
-    //    StdClcUndefined udef;
-    //    StdClcDouble dou;
-    //    StdClcString ss;
-    //    res += Stroka("\n") + udef.class_name()  + " " + dou.class_name() + " " + ss.class_name() + "\n";
-    //    res += Stroka(" Registered VARS:\n\n\n") + SavableClass::HelpForCategory("StdClcTypes_category");
-    //    Stroka str = DataSource::GenerateUnicName("tmp");
-    //    FilterTextOut tmp(str.c_str(),DataSource::Memory);
-    ////    tmp.SetRefMask(SavableClass::SingleFileStorage);
-    //    tmp.SetRefMask(SavableClass::UnlinkedObjects);
-    //    ClassDesc::WriteHelpOnCategory(tmp, "URS_Curve_category", ClassDesc::PrintHelp);
-    //    tmp<<"\n\nRegistered FreeE EOS classes \n";
-    //    ExEnv::set_out(&tmp);
-    //    ClassDesc::list_all_classes("MatterFreeE_category");
-    //    tmp<<"\n\nRegistered Caloric EOS classes \n";
-    //    ClassDesc::list_all_classes("Matter_category");
-    //    tmp<<"\n=====================================================\nDetailed help for matters\n";
-    //    ClassDesc::WriteHelpOnCategory(tmp,"MatterFreeE_category",ClassDesc::PrintHelp);
-    //    tmp<<"\n\n";
-    //    ClassDesc::WriteHelpOnCategory(tmp,"Matter_category",ClassDesc::PrintHelp);
-    //    ExEnv::set_out(&cout);
-    //	return Stroka(DataSource::GetStr(str.c_str()).c_str());
     return res;
 };
+void SetStack() {
+    const rlim_t kStackSize = 32 * 1024 * 1024;   // min stack size = 16 MB
+    struct rlimit rl;
+    int result;
+
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result != 0) {
+        log_always(Stroka("getrlimit returned result = ") + result);
+        return;
+    }
+    if (rl.rlim_cur > kStackSize) {
+        log_always(Stroka("current stack size ") + (int)rl.rlim_cur);
+        return;
+    }
+    rl.rlim_cur = kStackSize;
+    result = setrlimit(RLIMIT_STACK, &rl);
+    if (result != 0) log_always(Stroka("setrlimit returned result = ") + result);
+    log_always(Stroka("Settled stack size ") + (int)rl.rlim_cur);
+
+
+}
 int main(int argc, const char *argv[]) {
     SetLeakTest();
     Time_struct Time;
-    try {
-        NRef::CommandLineExe Cmd(
-            "Usage: urs_curve [Params_Key_Spec] [/][-]key  \n without parameters - standart test\n");
-        Cmd.Add('h', "PrintHelp", " - print this help \n");
-        Cmd.MainHelp += GenerateDetailedHelp();
-        Cmd.Analize(argc, argv);
-        //if (Cmd.Get("PrintDiscrepency").Exist) TestCase_Famessb_Static::PrintDiscrepencyEstimations=1;
-        //if (Cmd.Get("AllResults").Exist) TestCase_Famessb_Static::DeleteGoodResults=0;
-        if(Cmd.Argc == 1 || Cmd.Get("PrintHelp").Exist)
-            cout << Cmd.MakeHelp();
-        else
-            DrawCurve((int)Cmd.Argc, Cmd.Argv);
+    NRef::CommandLineExe Cmd(
+        "Usage: urs_curve [-]key  \n without parameters - help\n");
+    Cmd.Add('h', "PrintHelp", " - print this help \n");
+    Cmd.Add('d', "DebugLevel", " - sets debug level (-d3) \n");
+    Cmd.MainHelp += GenerateDetailedHelp();
+    Cmd.Analize(argc, argv);
+
+    if(Cmd.Argc == 1 || Cmd.Get("PrintHelp").Exist) {
+        cout << Cmd.MakeHelp();
+    } else {
+        if(Cmd.Get("DebugLevel").Exist) {
+            int level = min(NLogger::max_level - 1, atoi(Cmd.Get("DebugLevel").Val.c_str()));
+            log_always(Stroka("Setting Debug_level ") + NLogger::LevelNames[level] + ":" + level);
+            NLogger::TLogger::GetLogger().LogLevel = NLogger::ELevel(level);
+        }
+        DrawCurve((int) Cmd.Argc, Cmd.Argv);
     }
-    CATCHMAINEXCEPTION(" Urs_curve failed ");
 
     LeakTest();
     cout << "Done in " << Time << "\n";
