c5492a28437812e4312f11dceb6a75942d4d5eab
Alexey <pyal@mail.ru>
2017-03-18 03:06:58 -0700

    Restore depricated FreeEDis object - to test old code

--- cpp/URS/fre_dis.cpp
+++ cpp/URS/fre_dis.cpp
@@ -5,16 +5,16 @@
 //
 ////static ClassDesc FreeESumMatter_ClassDesc(typeid(FreeESumMatter),"FreeESumMatter",1,"MatterFreeE_category",create<FreeESumMatter>);
 ////static ClassDesc FreeEChemMatterMultiV_ClassDesc(typeid(FreeEChemMatterMultiV),"FreeEChemMatterMultiV",1,"MatterFreeE_category",create<FreeEChemMatterMultiV>);
-//static ClassDesc FreeEDis_ClassDesc(typeid(FreeEDis),"FreeEDis",1,"MatterFreeE_category",create<FreeEDis>);
-//static ClassDesc DisFindMinimE_ClassDesc(typeid(DisFindMinimE),"DisFindMinimE",1,"DisFindMinimE_category",create<DisFindMinimE>);
+static ClassDesc FreeEDis_ClassDesc(typeid(FreeEDis),"FreeEDis",1,"MatterFreeE_category",create<FreeEDis>); //DEPRICATED
+static ClassDesc DisFindMinimE_ClassDesc(typeid(DisFindMinimE),"DisFindMinimE",1,"DisFindMinimE_category",create<DisFindMinimE>);
 ////static ClassDesc DisFindH2Minim_ClassDesc(typeid(DisFindH2Minim),"DisFindH2Minim",1,"DisFindMinimE_category",create<DisFindH2Minim>);
-//static ClassDesc PureDis_FindE_ClassDesc(typeid(PureDis_FindE),"PureDis_FindE",1,"DisFindMinimE_category",create<PureDis_FindE>);
+static ClassDesc PureDis_FindE_ClassDesc(typeid(PureDis_FindE),"PureDis_FindE",1,"DisFindMinimE_category",create<PureDis_FindE>);
 //
 //
-//static ClassDesc Dis_RelatNum_MinimFunc_Single_ClassDesc(typeid(Dis_RelatNum_MinimFunc_Single),"Dis_RelatNum_MinimFunc_Single",1,"Dis_RelatNum_MinimFunc_category",create<Dis_RelatNum_MinimFunc_Single>);
+static ClassDesc Dis_RelatNum_MinimFunc_Single_ClassDesc(typeid(Dis_RelatNum_MinimFunc_Single),"Dis_RelatNum_MinimFunc_Single",1,"Dis_RelatNum_MinimFunc_category",create<Dis_RelatNum_MinimFunc_Single>);
 //static ClassDesc Dis_RelatNum_MinimFunc_SingleH2_ClassDesc(typeid(Dis_RelatNum_MinimFunc_SingleH2),"Dis_RelatNum_MinimFunc_SingleH2",1,"Dis_RelatNum_MinimFunc_category",create<Dis_RelatNum_MinimFunc_SingleH2>);
 //
-//static ClassDesc PureDis_Minim1Pres_ClassDesc(typeid(PureDis_Minim1Pres),"PureDis_Minim1Pres",1,"PureDis_MinimPres_category",create<PureDis_Minim1Pres>);
+static ClassDesc PureDis_Minim1Pres_ClassDesc(typeid(PureDis_Minim1Pres),"PureDis_Minim1Pres",1,"PureDis_MinimPres_category",create<PureDis_Minim1Pres>);
 
 
 //FreeESumMatter::FreeESumMatter()
