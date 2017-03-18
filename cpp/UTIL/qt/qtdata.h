b0d3df4e9c6f623f0a4217ab8e0d606652e540b6
Alexey <pyal@mail.ru>
2017-03-18 03:04:32 -0700

    Adding OneLineToMany option - to show multiple lines for one curve (if x continuity breaks

--- cpp/UTIL/qt/qtdata.h
+++ cpp/UTIL/qt/qtdata.h
@@ -41,6 +41,7 @@ class IFileData: public QtData::IDataSource {
     pair<string, vector<QtData::Data>> CurData;
     string NewDataName;
 public:
+    bool OneLineToMany = false;
     virtual pair<vector<string>, vector<double>> ReadHead() = 0;
     virtual bool isEnd(vector<double> &row_pnt) = 0;
     virtual string PlotName() = 0;
@@ -48,15 +49,19 @@ public:
     virtual bool getNext() {
         log_debug("Reading next data");
         auto head = ReadHead();
-        vector<vector<double>> points;
+        vector<vector<vector<double>>> points_arr(1);
         if(head.first.size() == 0) return false;
-        if(head.second.size() > 0) points.push_back(head.second);
+        if(head.second.size() > 0) points_arr.back().push_back(head.second);
         vector<double> row;
         int rowNum = 0;
+        double prevX = -1./M_MinDouble;
         while(!isEnd(row)) {
             if (row.size() == head.first.size()) {
-                points.push_back(row);
-//                log_always(Stroka("Adding row num: ") + rowNum + " content: " + Str::Vec2Str(row));
+                if(OneLineToMany && row.front() < prevX) {
+                    points_arr.push_back(vector<vector<double>>());
+                }
+                points_arr.back().push_back(row);
+                prevX = row.front();
             }
             else {
                 log_always(Stroka("Skipping row num: ") + rowNum + " content: " + Str::Vec2Str(row));
@@ -64,7 +69,16 @@ public:
             rowNum++;
         }
         CurData.first = PlotName();
-        CurData.second = QtData::BuildData(points, head.first);
+        int ar_num = 0;
+        for(auto &points: points_arr) {
+            auto names = head.first;
+            if(points_arr.size() > 1) {
+                for(auto &n:names) n = string(n, 0, 1) + "_" + to_string(ar_num);
+            }
+            auto pnts = QtData::BuildData(points, names);
+            for(auto &pnt: pnts) CurData.second.push_back(pnt);
+            ar_num++;
+        }
         return true;
     }
 };
@@ -74,9 +88,9 @@ class SingleFile: public IFileData {
     FILE *In;
     char FieldDelim;
 public:
-    SingleFile(const string &file, char fieldDelim):FileName(file),
+    SingleFile(const string &file, char fieldDelim, bool oneLineToMany):FileName(file),
        In(File::open(file.c_str(), "r", "SingleFile")), FieldDelim(fieldDelim) {
-
+            OneLineToMany = oneLineToMany;
     }
     ~SingleFile() {
         File::close(In);
@@ -85,17 +99,20 @@ public:
         return FileName;
     }
     virtual pair<vector<string>, vector<double>> ReadHead() {
-        double d;
-        pair<vector<string>, vector<double>> ret;
-        if (!File::GetLine(In, ret.first, FieldDelim)) return ret;
-        for(auto &str: ret.first) if(IsDouble(str.c_str(), d, 1)) ret.second.push_back(d);
-        if(ret.second.size() == ret.first.size()) {
-            for(int i = 0; i < ret.first.size(); i++)
-                ret.first[i] = ~(Stroka("c_") + i);
-        } else {
-            ret.second.clear();
-        }
-        return ret;
+        pair<vector<string>, vector<double>> prev, cur;
+        while(true) {
+            cur = ReadHead_int();
+            if(cur.first.size() == 0) return cur;
+            if(cur.second.size() != 0) {
+                if(prev.first.size() == cur.second.size()) {
+                    cur.first = prev.first;
+                    return cur;
+                }
+                return cur;
+            }
+            prev = cur;
+        };
+        return cur;
     };
     virtual bool isEnd(vector<double> &row_pnt) {
         vector<Stroka> tmp;
@@ -115,7 +132,22 @@ public:
         return false;
 
     }
-
+private:
+    pair<vector<string>, vector<double>> ReadHead_int() {
+        double d;
+        pair<vector<string>, vector<double>> ret;
+        do {
+            if (!File::GetLine(In, ret.first, FieldDelim)) return ret;
+        } while(ret.first.size() == 0);
+        for(auto &str: ret.first) if(IsDouble(str.c_str(), d, 1)) ret.second.push_back(d);
+        if(ret.second.size() == ret.first.size()) {
+            for(int i = 0; i < ret.first.size(); i++)
+                ret.first[i] = ~(Stroka("c_") + i);
+        } else {
+            ret.second.clear();
+        }
+        return ret;
+    };
 };
 
 
@@ -124,9 +156,9 @@ class MultiFile: public QtData::IDataSource {
     vector<Ref<SingleFile>> FileReaders;
     pair<string, vector<QtData::Data>> CurData;
 public:
-    MultiFile(const string &files, char fieldDelim): FileName(Str::SplitLine(Stroka(files.c_str()))) {
+    MultiFile(const string &files, char fieldDelim, bool oneLineToMany): FileName(Str::SplitLine(Stroka(files.c_str()))) {
         for(auto &name: FileName)
-            FileReaders.push_back(new SingleFile(name, fieldDelim));
+            FileReaders.push_back(new SingleFile(name, fieldDelim, oneLineToMany));
     }
     ~MultiFile(){}
     virtual pair<string, vector<QtData::Data>> &getData() {
--- cpp/UTIL/qt/qtshow.cpp
+++ cpp/UTIL/qt/qtshow.cpp
@@ -10,10 +10,11 @@ void Show(map<Stroka, Stroka> par) {
     vector<string> names;
     char fieldDelim(' ');
     if (par["FieldDelim"] != "SPACE") fieldDelim = par["FieldDelim"][0];
+    bool oneLineToMany = par["OneLineToMany"] != "1";
     log_always(Stroka("FieldDelim is [") + fieldDelim + "]");
     QtData::IDataSource *dataSource = (atoi(~par["gasd"]) == 0) ?
-                                      (QtData::IDataSource *)new MultiFile(par["file"], fieldDelim) :
-                                      (QtData::IDataSource *)new GasdData(par["file"], "CurTime", par["CurTimeFirst"] != "0");
+      (QtData::IDataSource *)new MultiFile(par["file"], fieldDelim, oneLineToMany) :
+      (QtData::IDataSource *)new GasdData(par["file"], "CurTime", par["CurTimeFirst"] != "0");
     QtPlot plot(dataSource);
     plot.Plot();
 }
@@ -22,7 +23,7 @@ int main(int argc, const char *argv[]) {
     Time_struct Time;
         NRef::TCommandParse Cmd(
                 "Usage: poly_test  - without parameters - help\n");
-        Cmd.Add(Show, "show", "plot file", "file in.dat plot file\ngasd 0 Gasd clc file\nCurTimeFirst 0  0 - wilkins format, 1 - kru format)\nFieldDelim SPACE", 1);
+        Cmd.Add(Show, "show", "plot file", "file in.dat plot file\ngasd 0 Gasd clc file\nCurTimeFirst 0  0 - wilkins format, 1 - kru format)\nFieldDelim SPACE\nOneLineToMany 1 If X point is less then previous - start new line ", 1);
         Cmd.SimpleRun(argc, argv);
 
     LeakTest();
